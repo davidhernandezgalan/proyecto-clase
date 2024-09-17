6e1cc3e5700411f652739b13929c4b82d4efb299
David Hernandez <david.hernandez7479@alumnos.udg.mx>
2024-09-17 15:41:40 -0600

    CRUD para noticias

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,12 @@
 <?php
 
 use App\Http\Controllers\ContactoController;
-use App\Models\Contacto;
+use App\Http\Controllers\NoticiaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/contacto/{tipo_usuario?}', [ContactoController::class, 'formularioContacto']);
 Route::post('/guardar-formulario', [ContactoController::class, 'guardarFormulario']);
 Route::get('/mensajes', [ContactoController::class, 'listado']);
+
+Route::resource('noticia', NoticiaController::class)->parameters(['noticia' => 'noticia']);
